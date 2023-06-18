9f0d84f2d6e5ae3af51e9df5e9e77b8669802483
wckang2147 <66623164+wckang2147@users.noreply.github.com>
2023-06-19 08:00:33 +0900

    Update loadingJar.java

--- ETC/loadingJar.java
+++ ETC/loadingJar.java
@@ -15,3 +15,18 @@ method.invoke( object, strTemp );
 // Loading Class
 File jarFile    = new File( "class package path" );      // 파일명이 아닌 폴더면만 기제
 URL[] urls = new URL[]{ new URL( file.toURL() ) };
+
+// Get current abs path
+public static void main(String args[]) {
+  String currentPaht = Paths.get("").toAbsolutePath().toString();
+}
+public static void main(String args[]) {
+  String currentPath = System.getProperty("user.dir");
+}
+public static void main(String args[]) {
+  String currentPath = 
+          FileSystems.getDefault().getPath("").toAbsolutePath().toString();
+}
+public static void main(String args[]) {
+  String currentPath = new File("").getAbsolutePath();
+}
